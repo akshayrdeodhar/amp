382e447b972448216ffb3c06866b689256a39cc0
Akshay Deodhar <bri9k@protonmail.com>
2020-02-03 10:52:08 +0530

    print to center of screen

--- assignment_1/realmode.asm
+++ assignment_1/realmode.asm
@@ -38,6 +38,16 @@ stringprint:
 	mov ah, 03h ; get curso position
 	int 10h     ; dl contains cursor column
 
+	; mov al, 01h ; set page 1 as the active page
+	; mov ah, 05h
+	; int 10h
+
+	mov ah, 02h
+	mov dh, 13
+	mov dl, 30
+	; set cursor to approximate center of screen
+	int 10h
+
 stringprint_loop_start:
 	inc dl
 	mov ah, 02h ; set cursor position
@@ -50,7 +60,7 @@ stringprint_loop_start:
 	mov ah, 09h	; character output
 	mov cx, 0001h	; print only once
 	mov bh, 00h 	; write to page 0
-	mov bl, 09h	; write character to screen with attribute
+	mov bl, 90h	; write character to screen with attribute
 	int 10h
 
 	jmp stringprint_loop_start
